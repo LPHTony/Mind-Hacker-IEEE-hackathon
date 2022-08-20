0a91d9d218bee3fa2bd61f70ae7aadb3822529d1
davidmingng <68263783+davidmingng@users.noreply.github.com>
2022-08-20 13:25:58 +0800

    Update Donation.sol

--- Donation.sol
+++ Donation.sol
@@ -1,30 +1,30 @@
 pragma solidity 0.8.7;
 
-contract VendingMachine {
+contract Donation {
 
-    // Declare state variables of the contract
-    address public owner;
-    mapping (address => uint) public cupcakeBalances;
+    address public owner; //charity of the fund raising event
+
+    struct ProjectProgress{
+        uint[] targets; //target of project progress to be met
+        uint[] donationToGive; //precentage of donation that will be given to the charities when each target is met
+        uint amountRaised; //Total among raised in the flag selling event
+        uint progressIndicator; //Quantiative factors that used to show the project progress (e.g. poverty rate, literacy rate)
+    }
+
 
     // When 'VendingMachine' contract is deployed:
     // 1. set the deploying address as the owner of the contract
     // 2. set the deployed smart contract's cupcake balance to 100
     constructor() {
         owner = msg.sender;
-        cupcakeBalances[address(this)] = 100;
     }
 
     // Allow the owner to increase the smart contract's cupcake balance
-    function refill(uint amount) public {
-        require(msg.sender == owner, "Only the owner can refill.");
-        cupcakeBalances[address(this)] += amount;
-    }
-
-    // Allow anyone to purchase cupcakes
-    function purchase(uint amount) public payable {
-        require(msg.value >= amount * 1 ether, "You must pay at least 1 ETH per cupcake");
-        require(cupcakeBalances[address(this)] >= amount, "Not enough cupcakes in stock to complete this purchase");
-        cupcakeBalances[address(this)] -= amount;
-        cupcakeBalances[msg.sender] += amount;
+    function requestTransfer(uint amount) public {
+        require(msg.sender == owner, "Only the owner can request fund transfer.");
+        //Update the progressIndicator from database or platform (such as Oracle)
+        //Check whether the indicator meets any targets
+        //If some target is met, the corresponding value in donationToGive will be sent to the Abelian wallet of the charity
+        //delete the fulfillled target and donation given in the array
     }
 }
